0db474fc1c70a7da8d9ce7cb53f3c39de7caa5b1
Qiaojing Yan <qiaojingyan9@gmail.com>
2015-11-12 20:46:27 -0800

    write comments for softmax

--- softmax.py
+++ softmax.py
@@ -2,6 +2,7 @@ from baseline import *
 from util import *
 import math
 
+# Calculating the probability of belonging to each class, given weights and features
 def classProbs(weights, features):
   probs = []
   k = len(weights)
@@ -12,14 +13,18 @@ def classProbs(weights, features):
     probs[i] = probs[i] * 1.0/s
   return probs
 
+# Using softmax to training on data, print training error and test error, return weights
 def train(trainExamples, testExamples, featureExtractor):
   print "=" * 30
   print "Doing Softmax"
-  alpha = 0.1
+  alpha = 0.01      # learning rate
   weights = []
+  # There are a total of five classes: 1 star, 2 star, ... 5 star. Each has its only weights. 
+  # These weights are stored in a list of length 5. 
   for i in range(5):
     weights.append({})
-  numIters = 5
+  numIters = 5     # number of iterations through test examples
+  # Doing stachastic gradient descent
   for iter in range(numIters):
     print "iteration   ", iter+1
     for (x, y) in trainExamples:
@@ -34,12 +39,15 @@ def train(trainExamples, testExamples, featureExtractor):
       for i in range(5):
         increment(N[i], -probs[i], features)
       for i in range(5):
-        increment(weights[i], 0.01, N[i])
+        increment(weights[i], alpha, N[i])
+  
+  # calculate training error
   trainError = evaluatePredictor(trainExamples, lambda(x) : max((prob, k) for (k, prob) in enumerate(classProbs(weights, featureExtractor(x))))[1] + 1)
   print "=" * 30
   print "Result"
   print 'Iter : %s   Training error = %s' % (iter+1, trainError)
-
+  
+  # calculate test error
   """
   testError = evaluatePredictor(testExamples, lambda(x) : max((prob, k) for (k, prob) in enumerate(classProbs(weights, featureExtractor(x))))[1] + 1)
   print "=" * 30
